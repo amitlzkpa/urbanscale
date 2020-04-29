49086f126399b679fd05cbd9b071903d962faaa5
amitlzkpa <amit.lzkpa@gmail.com>
2020-04-29 18:28:09 -0500

    customise for my starter files

---
+++ src/components/vuewwebapp.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>vuewwebapp</h1>
+    <p>An app to reuse in the future (hopefully).</p>
+    <img v-bind:src="'/imgs/img.png'" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'vuewwebapp'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
