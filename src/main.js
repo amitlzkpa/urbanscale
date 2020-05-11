d6ba3932eef998b877b329e4b0adaf4c83e25d4f
amitlzkpa <amit.lzkpa@gmail.com>
2020-05-11 02:28:41 -0500

    add auth0 login; working

--- src/main.js
+++ src/main.js
@@ -1,6 +1,21 @@
 import Vue from 'vue';
-import App from './App.vue';
-import router from './router';
+import App from '@/App.vue';
+import router from '@/router';
+
+import { domain, clientId } from '@/configs/auth.json';
+import { Auth0Plugin } from "./auth";
+
+Vue.use(Auth0Plugin, {
+  domain,
+  clientId,
+  onRedirectCallback: appState => {
+    router.push(
+      appState && appState.targetUrl
+        ? appState.targetUrl
+        : window.location.pathname
+    );
+  }
+});
 
 Vue.config.productionTip = false;
 
--- src/partials/Navbar.vue
+++ src/partials/Navbar.vue
@@ -2,13 +2,26 @@
   <div>
     <router-link to="/">Home</router-link>
     &nbsp;|&nbsp;
-    <router-link to="/login">Login</router-link>
+    <span v-if="!$auth.loading">
+      <a v-if="!$auth.isAuthenticated" @click="login">Log in</a>
+      <a v-if="$auth.isAuthenticated" @click="logout">Log out({{ $auth.user.name }})</a>
+    </span>
   </div>
 </template>
 
 <script>
 export default {
-
+  name: 'Navbar',
+  methods: {
+    login() {
+      this.$auth.loginWithRedirect();
+    },
+    logout() {
+      this.$auth.logout({
+        returnTo: window.location.origin
+      });
+    }
+  }
 }
 </script>
 
