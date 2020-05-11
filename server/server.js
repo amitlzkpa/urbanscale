2a963799d05d5cb865aad0b35dce0c9d3296178b
amitlzkpa <amit.lzkpa@gmail.com>
2020-05-11 18:51:01 -0500

    remove auth.json config file; store vars on backend and get on client using get request

--- server/server.js
+++ server/server.js
@@ -5,11 +5,22 @@ const bodyParser = require('body-parser');
 const DIR = 'dist';
 const PORT = process.env.PORT || 8080;
 
+const AUTH0_DOMAIN = process.env.AUTH0_DOMAIN;
+const AUTH0_CLIENT_ID = process.env.AUTH0_CLIENT_ID;
+
 const app = express();
 app.use(express.static(DIR));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+app.get('/api/auth0-secrets', function(req, res) {
+  let creds = {
+    AUTH0_DOMAIN: AUTH0_DOMAIN,
+    AUTH0_CLIENT_ID: AUTH0_CLIENT_ID
+  };
+  res.json(creds);
+});
 
 app.get('/api/foo', function(req, res) {
   console.log('foo');
--- src/main.js
+++ src/main.js
@@ -2,25 +2,35 @@ import Vue from 'vue';
 import App from '@/App.vue';
 import router from '@/router';
 
-import { domain, clientId } from '@/configs/auth.json';
 import { Auth0Plugin } from "./auth";
 
-Vue.use(Auth0Plugin, {
-  domain,
-  clientId,
-  onRedirectCallback: appState => {
-    router.push(
-      appState && appState.targetUrl
-        ? appState.targetUrl
-        : window.location.pathname
-    );
-  }
-});
+async function main() {
 
-Vue.config.productionTip = false;
+  let resp = await fetch('/api/auth0-secrets');
+  let secrets = await resp.json();
+  let domain = secrets.AUTH0_DOMAIN;
+  let clientId = secrets.AUTH0_CLIENT_ID;
 
-new Vue({
-  router,
-  render: h => h(App),
-}).$mount('#app');
+  Vue.use(Auth0Plugin, {
+    domain,
+    clientId,
+    onRedirectCallback: appState => {
+      router.push(
+        appState && appState.targetUrl
+          ? appState.targetUrl
+          : window.location.pathname
+      );
+    }
+  });
+  
+  Vue.config.productionTip = false;
+  
+  new Vue({
+    router,
+    render: h => h(App),
+  }).$mount('#app');
 
+
+}
+
+main();
