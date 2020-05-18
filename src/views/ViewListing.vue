5d118294e0929a593a33df6a32a720e30312e5a6
amitlzkpa <amit.lzkpa@gmail.com>
2020-05-18 01:22:07 -0500

    bring in portis

--- src/views/ViewListing.vue
+++ src/views/ViewListing.vue
@@ -15,7 +15,13 @@
 
 <script>
 import axios from 'axios';
+import Portis from '@portis/web3';
+import Web3 from 'web3';
+
 import ListingCard from '@/components/ListingCard.vue';
+
+const portis = new Portis('c9972761-699b-441e-a522-56b5bc729b65', 'mainnet');
+const portisWeb3 = new Web3(portis.provider);
 
 export default {
   name: 'ViewListing',
@@ -32,6 +38,7 @@ export default {
     let resp = await axios.get(`/api/listing/cusipNo/${this.$route.params.cusipNo}`);
     this.listing = resp.data;
     this.ready = true;
+    console.log(portisWeb3);
   },
   methods: {
   }
