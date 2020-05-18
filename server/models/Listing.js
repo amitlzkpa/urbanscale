3f6f31e29a08df835a0afcfb35574bb27d05a23f
amitlzkpa <amit.lzkpa@gmail.com>
2020-05-17 22:42:42 -0500

    add listing model

---
+++ server/models/Listing.js
@@ -0,0 +1,58 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+const ListingSchema = new mongoose.Schema({
+  createdBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  },
+  name: {
+    type: String,
+    index: true,
+    default: ""
+  },
+  cusipNo: {
+    type: String,
+    index: true,
+    default: ""
+  },
+  emmaId: {
+    type: String,
+    index: true,
+    default: ""
+  },
+  maturityDate: {
+    type: Date,
+    index: true,
+    default: null
+  },
+  principal: {
+    type: Number,
+    index: true,
+    default: null
+  },
+  coupon: {
+    type: Number,
+    index: true,
+    default: null
+  },
+  tokenSupply: {
+    type: Number,
+    index: true,
+    default: null
+  },
+  description: {
+    type: String,
+    default: ""
+  },
+  location: {
+    type: String,
+    default: ""
+  },
+  contractAddress: {
+    type: String,
+    default: ""
+  },
+}, {timestamps: true});
+
+module.exports = mongoose.model('Listing', ListingSchema);
