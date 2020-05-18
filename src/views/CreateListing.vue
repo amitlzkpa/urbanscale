0e05e88a8a281caf9acce53b0ff52a51613dc916
amitlzkpa <amit.lzkpa@gmail.com>
2020-05-17 23:25:08 -0500

    update profile page with card; try to init metamask in create listing

--- src/views/CreateListing.vue
+++ src/views/CreateListing.vue
@@ -167,8 +167,9 @@ export default {
       tokenSupply: 0
     }
   },
-  created() {
+  async created() {
     if(window.ethereum) {
+      await window.ethereum.enable();
       web3 = new Web3(window.web3.currentProvider);
     }
   },
--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,19 +1,45 @@
 <template>
   <div>
-    <h1>Profile</h1>
-    <img :src="$auth.user.picture" />
-    <p>
-      Name: {{ $auth.user.name }}
-    </p>
-    <p>
-      Email: {{ $auth.user.email }}
-    </p>
+
+    <br />
+    
+    <div class="card">
+
+      <div class="card-content">
+
+          <div class="columns">
+
+            <div class="column is-narrow">
+              <figure class="image is-128x128">
+                <img class="is-rounded" :src="$auth.user.picture" />
+              </figure>
+            </div>
+
+            <div class="column">
+              <p class="is-size-4 has-text-weight-bold">
+                {{ $auth.user.name }}
+              </p>
+              <i class="has-text-grey">
+                {{ $auth.user.nickname }}
+              </i>
+              <p>
+                {{ $auth.user.email }}
+              </p>
+            </div>
+
+          </div>
+
+      </div>
+
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-
+  created() {
+    console.log(this.$auth.user);
+  }
 }
 </script>
 
